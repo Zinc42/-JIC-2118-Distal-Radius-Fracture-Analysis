d0ebc318df416de23e4b92fdb273396ffb55de23
D Rogers <75626158+darienr1999@users.noreply.github.com>
2022-09-27 14:40:44 -0400

    added initial camera roll screen layout

--- lib/camera_roll_screen.dart
+++ lib/camera_roll_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:image_picker/image_picker.dart';
+
 import 'dart:io';
 import "screen_button.dart";
 
@@ -17,20 +17,7 @@ class _CameraRollScreenState extends State<CameraRollScreen> {
   @override
   File? image;
 
-  Future pickImage() async {
-    try {
-      final imagePicked =
-          await ImagePicker().pickImage(source: ImageSource.gallery);
-      if (imagePicked == null) return;
-      final temporaryImageFile = File(imagePicked.path);
-      setState(() {
-        this.image = temporaryImageFile;
-      });
-    } on PlatformException catch (e) {
-      //To do: Handle exception when user does not allow permission to camera roll
-      print("Failed to pick image: $e");
-    }
-  }
+
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,20 +37,11 @@ class _CameraRollScreenState extends State<CameraRollScreen> {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Flexible(
-                child: ScreenButton(
-                    buttonText: "Select an Image",
-                    pressFunction: () {
-                      pickImage();
-                    }),
-              ),
+
               SizedBox(
                 width: 10,
               ),
-              Flexible(
-                child:
-                    ScreenButton(buttonText: "Confirm", pressFunction: () {}),
-              ),
+
             ],
           )
 
