ebac9f758e397cbcf1c1450f570932fc5762f884
Andrew Zhang <azhang377@gatech.edu>
2022-09-28 21:04:27 -0400

    resolve merge errors

--- lib/image_upload_screen.dart
+++ lib/image_upload_screen.dart
@@ -86,94 +86,55 @@ class _ImageUploadScreenView extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 35.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Stack(
-              alignment: Alignment.center,
-              children: const [
-                Positioned(left: 10, child: BackButton()),
-                Align(
-                    child: Text(
-                  "Choose Analysis Type",
-                  textAlign: TextAlign.center,
-                  textScaleFactor: 1.5,
-                )),
-              ],
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  ScreenButton(
-                    buttonText: "From Camera",
-                    pressFunction: state.toCameraScreen,
-                  ),
-                  ScreenButton(
-                    buttonText: "From Camera Roll",
-                    pressFunction: state.toCameraRollScreen
-                  ),
-                  ScreenButton(
-                    buttonText: "From Files",
-                    pressFunction: () {
-                      print("Files");
-                      // Implementation TBD
-                    },
-                  ),
-                  ScreenButton(
-                    buttonText: "From Google Drive",
-                    pressFunction: () {
-                      print("Google Drive");
-                      // Implementation TBD
-                  },
-                ),
-              ],
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  ScreenButton(
-                    buttonText: "From Camera",
-                    pressFunction: () {
-                      print("Camera");
-                      toCameraScreen();
-                      //Code to navigate to next screen goes here
-                      //might look like navigator.push(context, static id of next screen when its made
-                    },
-                  ),
-                  ScreenButton(
-                    buttonText: "From Camera Roll",
-                    pressFunction: () {
-                      print("to camera roll");
-                      Navigator.pushNamed(context, CameraRollScreen.id);
-                      //Code to navigate to next screen goes here
-                      //might look like navigator.push(context, static id of next screen when its made
-                    },
-                  ),
-                  ScreenButton(
-                    buttonText: "From Files",
-                    pressFunction: () {
-                      print("Files");
-                      //Code to navigate to next screen goes here
-                      //might look like navigator.push(context, static id of next screen when its made
-                    },
-                  ),
-                  ScreenButton(
-                    buttonText: "From Google Drive",
-                    pressFunction: () {
-                      print("Google Drive");
-                      //Code to navigate to next screen goes here
-                      //might look like navigator.push(context, static id of next screen when its made
-                    },
-                  ),
+          margin: const EdgeInsets.symmetric(vertical: 35.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Stack(
+                alignment: Alignment.center,
+                children: const [
+                  Positioned(left: 10, child: BackButton()),
+                  Align(
+                      child: Text(
+                    "Choose Analysis Type",
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 1.5,
+                  )),
                 ],
               ),
-            ),
-          ],
-        ),
-    ),
-      ));
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ScreenButton(
+                      buttonText: "From Camera",
+                      pressFunction: state.toCameraScreen,
+                    ),
+                    ScreenButton(
+                      buttonText: "From Camera Roll",
+                      pressFunction: state.toCameraRollScreen
+                    ),
+                    ScreenButton(
+                      buttonText: "From Files",
+                      pressFunction: () {
+                        print("Files");
+                        // Implementation TBD
+                      },
+                    ),
+                    ScreenButton(
+                      buttonText: "From Google Drive",
+                      pressFunction: () {
+                        print("Google Drive");
+                        // Implementation TBD
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ]
+          ),
+        )
+      )
+    );
   }
 }
